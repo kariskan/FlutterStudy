8180bf8d3e7e800b7d9d91d08f51057038261636
kariskan <73534426+kariskan@users.noreply.github.com>
2021-05-30 20:26:51 +0900

    3-BasicCode

---
+++ study/3-BasicCode.dart
@@ -0,0 +1,40 @@
+import "package:flutter/material.dart";
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  //const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "First app",
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  //const ({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("First app"),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text("Hello"),
+            Text("Hello"),
+            Text("Hello")
+          ],
+        ),
+      ),
+    );
+  }
+}
